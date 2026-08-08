a70c417ffc34de0aa372300eef41c022e8d6d117
sd-projet <seneboudiarrapro@gmail.com>
2026-08-08 18:43:22 +0200

    Fix Symfony cache permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 mkdir -p /var/www/html/var/cache
 mkdir -p /var/www/html/var/log
 
+php bin/console cache:clear --env=prod
+
 chown -R www-data:www-data /var/www/html/var
 chmod -R 775 /var/www/html/var
-
-php bin/console cache:clear --env=prod
 
 exec apache2-foreground
