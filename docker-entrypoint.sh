9e78cd07f88b38ea16844b01947aaf383dcbd6a4
sd-projet <seneboudiarrapro@gmail.com>
2026-08-08 18:39:04 +0200

    upd aiven

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -e
 
-php bin/console cache:clear --env=prod || true
+mkdir -p /var/www/html/var/cache
+mkdir -p /var/www/html/var/log
+
+chown -R www-data:www-data /var/www/html/var
+chmod -R 775 /var/www/html/var
+
+php bin/console cache:clear --env=prod
 
 exec apache2-foreground
